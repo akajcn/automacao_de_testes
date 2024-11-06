c26026b99e9fbc2eca79f64e2dabd80545671b1a
akajcn <akajcn@gmail.com>
2024-11-06 09:40:30 -0300

    feat: adição do opção chrome

--- ferramentas/02_selenium_exercise/test_exercise_to_solve.py
+++ ferramentas/02_selenium_exercise/test_exercise_to_solve.py
@@ -11,7 +11,7 @@ import time
 import pathlib
 
 # Configuração para o navegador Chrome
-# navegador = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+navegador = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 def executar_teste_de_pagina():
     # Abrindo a página HTML local para execução dos testes
